4f48c9d6c9a0d68ece9446316278601fe92a4b3c
velveet <velveet@pm.me>
2020-09-24 17:30:25 -0500

    services text added.

--- client/pages/services.jsx
+++ client/pages/services.jsx
@@ -4,6 +4,7 @@ const Services = () => {
     return (
         <Container title={'Next.js Project - Services'}>
             <h1>Services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nam porro nisi delectus qui perferendis dicta ea soluta a fuga, eum corporis harum eius quod necessitatibus sequi expedita quis quibusdam libero iusto?</p>
         </Container>
     )
 }
