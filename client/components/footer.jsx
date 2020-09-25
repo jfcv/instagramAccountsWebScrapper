8a6f7e22ae5414204f04a7476675b8f2b3ce6320
velveet <velveet@pm.me>
2020-09-24 20:04:18 -0500

    [about lorem added], [footer styling]

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -1,10 +1,17 @@
 
 const Footer = () => {
     return(
-        <div>
-            <h2>Copyrighted</h2>
-        </div>
+        <footer className="text-center p-2" style={footerStyle}>
+            &copy; Snappr | Copyright 2020
+        </footer>
     )
 }
 
 export default Footer
+
+const footerStyle = {
+    position: 'absolute',
+    bottom: '0',
+    width: '100%',
+    borderTop: '2px dotted',
+}
--- client/pages/about.jsx
+++ client/pages/about.jsx
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <Container title={'Next.js Project - About'}>
             <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus exercitationem voluptatem ea earum dolore, enim voluptas iure aliquid odio accusantium animi ullam, perferendis fugiat. Exercitationem dolorum ab consequatur totam non?</p>
         </Container>
     )
 }
