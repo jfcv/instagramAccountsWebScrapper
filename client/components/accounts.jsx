c3969f97bcb1f9cb7eef0bf3cfb457686b7e0bc9
velveet <velveet@pm.me>
2020-09-27 18:05:02 -0500

    [tracking all the accounts on the UI], [getting each account url from the document itself]

--- client/components/accounts.jsx
+++ client/components/accounts.jsx
@@ -5,7 +5,9 @@ const Accounts = ({accounts}) => {
     const onTrack = async (e) => {
         try {
             e.preventDefault();
-            const response = await axios.post('http://localhost:5000/mail',{account: 'blue'});
+            console.log(document.getElementById('accountUrl').innerText);
+            const accountUrl = document.getElementById('accountUrl').innerText;
+            const response = await axios.post('http://localhost:5000/mail',{account: accountUrl});
             console.log(response.data);
         } catch (err) {
             console.error(err.message);
@@ -18,7 +20,7 @@ const Accounts = ({accounts}) => {
                 accounts.map(account => (
                 <li className="list-group-item list-group-item-action" key={account.id}>
                     <div className="row ml-2">
-                        <h5>{account.id} - {account.account}</h5>
+                        <h5 className="d-flex">{account.id}-<div id="accountUrl">{account.account}</div></h5>
                         <button type="button" className="btn btn-outline-secondary ml-auto mr-4" onClick={onTrack}>Track Account</button>
                     </div>
                     
