b34ff286ec204705fa28db6f6f2987b2a98cd435
velveet <velveet@pm.me>
2020-09-27 20:51:12 -0500

    [sending single identified URLs depending on the account clicked]

--- client/components/accounts.jsx
+++ client/components/accounts.jsx
@@ -2,27 +2,33 @@ import axios from 'axios'
 
 const Accounts = ({accounts}) => {
 
-    const onTrack = async (e) => {
-        try {
-            e.preventDefault();
-            const accountUrl = document.getElementById('accountUrl').innerText;
-            const response = await axios.post('http://localhost:5000/mail',{account: accountUrl});
-            console.log(response.data);
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
     return (
         <ul className="mt-5">
             {
                 accounts.map(account => (
                 <li className="list-group-item list-group-item-action" key={account.id}>
                     <div className="row ml-2">
-                        <h5 className="d-flex">{account.id}-<div id="accountUrl">{account.account}</div></h5>
-                        <button type="button" className="btn btn-outline-secondary ml-auto mr-4" onClick={onTrack}>Track Account</button>
+
+                        <h5 className="d-flex">{account.id}-<div id={`accountUrl/${account.id}`}>{account.account}</div></h5>
+
+                        <button 
+                            type="button" 
+                            className="btn btn-outline-secondary ml-auto mr-4" 
+                            onClick={async (e) => {
+                                    try {
+                                        e.preventDefault();
+                                        const accountUrl = document.getElementById(`accountUrl/${account.id}`).innerText;
+                                        const response = await axios.post('http://localhost:5000/mail',{account: accountUrl});
+                                        console.log(response.data);
+                                    } catch (err) {
+                                        console.error(err.message);
+                                    }
+                            }} 
+                        >
+                                Track Account
+                        </button>
+
                     </div>
-                    
                 </li>
                 ))
             }
